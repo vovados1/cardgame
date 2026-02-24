786a62ba0d13db6ae4024b633ed11641c2b908f7
stanislav554pc <staskopylchuk@gmail.com>
2026-02-24 11:06:35 -0600

    background, gamephysics, sound, animations and textures

--- server2.js
+++ server2.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'solitaire.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
